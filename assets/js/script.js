4f4a2f84b1a35b9a164d2655ac05e555aed8b145
Maxwell Sor <atm2chx3@students.codeinstitute.net>
2024-10-18 11:04:29 +0000

    Add text questions for the quiz

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+const quiz = [
+    {
+        question: "What mechanical tool is used for tigthening a 13-size bolt?",
+        answers: [
+            {text: "screw driver", correct: false},
+            {text: "13-spanner", correct: True},
+            {text: "hammer", correct: false},
+        ]
+    },
+    {
+        question: "A G-clamp is a tool used in the workshop to hold components when filing or cuting.",
+        answers: [
+            {text: "Yes", correct: True},
+            {text: "No", correct: false},
+            {text: "None", correct: false},
+        ]  
+    },
+    {
+        question: "Pliers are simple tools used to hold electrical cables when working on an electrical component",
+        answers: [
+            {text: "No", correct: false},
+            {text: "None", correct: false},
+            {text: "Yes", correct: True},
+        ]
+    },
+    {
+        question: "A hammer is a simple tool used in the workshop to make work easier and faster.",
+        answers: [
+            {text: "Yes", correct: True},
+            {text: "No", correct: false},
+            {text: "None", correct: false},
+        ]
+    },
+    {
+        question: "What simple mechanical tool is used for tigthening and loosening of a screw?",
+        answers: [
+            {text: "screw driver", correct: True},
+            {text: "13-spanner", correct: false},
+            {text: "hammer", correct: false},
+        ]
+    }
+]
