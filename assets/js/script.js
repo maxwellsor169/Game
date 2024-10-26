876d5458c6a9a890f345718b827191e25f90f00d
Maxwell Sor <atm2chx3@students.codeinstitute.net>
2024-10-26 12:51:03 +0000

    Correct some bugs in the index HTML and Java file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,32 @@
 /** 
+ * The following HTML codes were written under the inspiration and guide of a code institute student project sample
+ * This code is to load the welcome page before the quiz page */
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    const guideButton = document.getElementById("guide");
+    const guideElement = document.getElementById("guide-content");
+    const closeButton = document.getElementById("close");
+
+    guideButton.addEventListener("click", function() {
+
+        if(guideElement.style.display === "none") {
+            guideElement.style.display = "block";
+        } else {
+            guideElement.style.display = "block";
+        }
+    });
+
+    closeButton.addEventListener("click", function() {
+        guideElement.style.display = "none";
+    });
+})
+
+
+
+
+
+/** 
  * Code below was written from a tutorial guide on https://www.youtube.com/watch?v=PBcqGxrr9g8&t=179s
  *  DOM Elements declaration */
 
